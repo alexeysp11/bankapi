05efa9a33cd484b9573c704a54663d3c4976c36e
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-05-01 20:03:59 +0500

    edited build file

--- _build.cmd
+++ _build.cmd
@@ -1,5 +1,3 @@
 if not exist bin mkdir "bin"
 if not exist bin/configs mkdir "bin\configs"
-go build -o bin/bankapiservice.exe src/main.go src/server.go src/client.go src/confighelper.go 
-copy configs\*.* "bin"
-move bin\appsettings.json "bin\configs\appsettings.json"
+go build -o bin/bankapiservice.exe src/main.go src/server.go src/client.go src/confighelper.go && copy configs\*.* "bin" && move bin\appsettings.json "bin\configs\appsettings.json"
